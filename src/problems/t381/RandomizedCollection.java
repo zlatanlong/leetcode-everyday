a2827158767c5a3963a9493527b99b636f371518
zlatanlong <928339761@qq.com>
2020-10-31 14:52:08 +0800

    1031

---
+++ src/problems/t381/RandomizedCollection.java
@@ -0,0 +1,66 @@
+package problems.t381;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+
+public class RandomizedCollection {
+    Map<Integer, Set<Integer>> idx;
+    List<Integer> nums;
+
+    /** Initialize your data structure here. */
+    public RandomizedCollection() {
+        idx = new HashMap<Integer, Set<Integer>>();
+        nums = new ArrayList<Integer>();
+    }
+    
+    /** Inserts a value to the collection. Returns true if the collection did not already contain the specified element. */
+    public boolean insert(int val) {
+        nums.add(val);
+        Set<Integer> set = idx.getOrDefault(val, new HashSet<Integer>());
+        set.add(nums.size() - 1);
+        idx.put(val, set);
+        return set.size() == 1;
+    }
+    
+    // 主要难度在删除的空间复杂度，不能查找元素删除，只能删除索引，
+    // 而为了删除一个值时其他值索引不变，直接把他和最后一个值交换位置和索引，从而删最后一个达成目的
+    /** Removes a value from the collection. Returns true if the collection contained the specified element. */
+    public boolean remove(int val) {
+        if (!idx.containsKey(val)) {
+            return false;
+        }
+        Iterator<Integer> it = idx.get(val).iterator();  
+        int i = it.next();
+        int lastNum = nums.get(nums.size() - 1);
+        nums.set(i, lastNum);
+        idx.get(val).remove(i);
+        idx.get(lastNum).remove(nums.size() - 1);
+        if (i < nums.size() - 1) {
+            idx.get(lastNum).add(i);
+        }
+        if (idx.get(val).size() == 0) {
+            idx.remove(val);
+        }
+        nums.remove(nums.size() - 1);
+        return true;
+    }
+    
+    /** Get a random element from the collection. */
+    public int getRandom() {
+        return nums.get((int) (Math.random() * nums.size()));
+    }
+}
+
+/**
+ * Your RandomizedCollection object will be instantiated and called as such:
+ * RandomizedCollection obj = new RandomizedCollection();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
